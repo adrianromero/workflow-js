517a388532c34ed0a945d7a67a52eecbf2569b8d
Adrián Romero <adrianromero@users.noreply.github.com>
2015-08-25 19:55:41 +0200

    put code pretty

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@
 var mymath = require('./mymath');
 var _ = require('underscore');
 
-console.log('Hello Node JS!!!');
+console.log('Hello Javascript!!!');
 console.log('Calculate 2 + 2 = ' + mymath.add(2, 2));
 console.log('Random number between 1 and 100 = ' + _.random(1, 100));
