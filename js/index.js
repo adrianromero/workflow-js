fb9cc54d6264bc441263b06f0c9cb978cc26d402
Adrián Romero <adrianromero@users.noreply.github.com>
2015-09-14 00:35:01 +0200

    Using console parameters

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ var mymath = require('./mymath');
 var _ = require('underscore');
 
 console.log('Hello Javascript!!!');
-console.log('Calculate 2 + 2 = ' + mymath.add(2, 2));
-console.log('Random number between 1 and 100 = ' + _.random(1, 100));
+console.log('Calculate 2 + 2 = %d.', mymath.add(2, 2));
+console.log('Random number between 1 and 100 = %d.', _.random(1, 100));
